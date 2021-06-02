96b957a9513b53f08bc5fca51bcbe876999a680b
Mafia-1911 <74264555+Mafia-1911@users.noreply.github.com>
2021-06-02 18:41:59 +0600

    Add files via upload

---
+++ AddContact.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class AddContact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Add Contact</h2>
+                <form action="" className="ui form">
+                    <div className="field"> <label >Name</label>   
+                        <input type="text" name="name" id="" placeholder="name" /> 
+                    </div>
+                    <div className="field"> <label >Email</label>   
+                        <input type="text" Email="name" id="" placeholder="email" /> 
+                    </div>
+                   
+            <button className="ui button blue"></button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddContact;
