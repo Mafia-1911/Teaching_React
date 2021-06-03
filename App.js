a130e2a7d933db8f41e09d6f5310f441d236a91f
Mafia-1911 <74264555+Mafia-1911@users.noreply.github.com>
2021-06-03 22:30:27 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './components/Header'
+import AddContact from './components/AddContact'
+
+
+function App() {
+  return (
+    <div>
+
+      <AddContact/>
+  </div>
+    
+  );
+}
+
+export default App;
